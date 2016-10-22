b3c709ae1f5fb39af09a7ff8c340c0811cb137cd
manukarthik <manukarthikkr@gmail.com>
2016-10-21 19:31:59 -0500

    installed gulp and gutil

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,17 @@
 var gulp=require('gulp'),
-gutil=require('gulp-util');
-gulp.task('log',function(){
+	gutil=require('gulp-util'),
+	coffee=require('gulp-coffee');
 
-gutil.log('im manu');
+var coffeeSources = ['components/coffee/tagline.coffee'];
+	gulp.task('coffee',function(){
+	gulp.src(coffeeSources)
+	.pipe(coffee({bare:true})
+	.on('error',gutil.log))
+	.pipe(gulp.dest('components/scripts'))
+
+
+	
+
 
 
 });
